3a1bac8f4612f8a994c830937bca5253dfad4b0c
delibash <interlekt@gmail.com>
2025-05-20 09:32:27 -0400

    chore: dump unused files

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { getProjects } from '../../lib/api';
 import Layout from '../../components/Layout';
-import Button from '../../components/UI/Button';
+// import Button from '../../components/UI/Button';
 
 export default function Home() {
   const [recentProjects, setRecentProjects] = useState([]);
